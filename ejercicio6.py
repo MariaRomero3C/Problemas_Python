46da98189b8e98fe091124d3bc96dbde596ed18b
MariaRomero3C <114919286+MariaRomero3C@users.noreply.github.com>
2023-05-29 13:26:48 +0200

    Add files via upload

---
+++ ejercicio6.py
@@ -0,0 +1,7 @@
+num1 = int(input("Primer número: "))
+num2 = int(input("segundo número: "))
+num3 = int(input("Tercer número: "))
+
+print("La multiplicación de:", num1,",", num2,"y",num3,"es igual a: ")
+
+print(num1*num2*num3)
